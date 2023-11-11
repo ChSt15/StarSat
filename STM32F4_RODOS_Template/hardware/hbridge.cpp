81c7ac5eb7206532cfd8609a8755fc43c1c03048
DerDax7 <maxi.roth@me.com>
2023-11-11 12:40:43 +0100

    Hbridge skeleton done

---
+++ STM32F4_RODOS_Template/hardware/hbridge.cpp
@@ -0,0 +1,15 @@
+#include "hbridge.hpp"
+
+
+HBridge::HBridge(int frequency, int increments, PWM_IDX pwm1_id, PWM_IDX pwm2_id)
+{
+
+}
+
+void HBridge::setVoltage(float voltage)
+{
+
+}
+
+
+HBridge hbridge;
