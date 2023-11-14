e7a031a0c223481c8b401889277f8f3d3903df2a
Joshua Redelbach <joshua.redelbach@t-online.de>
2023-11-14 16:32:15 +0100

    Added IMU Calibration Thread

---
+++ STM32F4_RODOS_Template/control/CalibrationIMU.cpp
@@ -0,0 +1,56 @@
+#include "CalibrationIMU.hpp"
+
+#include "rodos.h"
+
+
+void CalibrationIMU::init(){
+
+}
+
+
+void CalibrationIMU::run(){
+
+    /**
+     * Rough idea
+    */
+
+   while(true){
+        suspendCallerUntil(NOW() + this->modePeriod * MILLISECONDS);
+
+        // Update this->calibMode
+
+        switch (this->calibMode)
+        {
+        case CalibAccel:
+            const IMUCalib calibDataAccel = calibrateAccel();
+            imu.setAccelCalib(calibDataAccel);
+            break;
+        case CalibGyro:
+            const IMUCalib calibDataGyro = calibrateGyro();
+            imu.setGyroCalib(calibDataGyro);
+            break;
+        case CalibMag:
+            const IMUCalib calibDataMag = calibrateMag();
+            imu.setMagCalib(calibDataMag);
+            break;
+        case DoNothing:
+            break;
+        }
+   }
+
+}
+
+
+IMUCalib CalibrationIMU::calibrateAccel(){
+
+}
+
+
+IMUCalib CalibrationIMU::calibrateMag(){
+
+}
+
+
+IMUCalib CalibrationIMU::calibrateGyro(){
+
+}
