b3f4fbe623893c23733bb6296b2574a65a1b10fb
JoshRedelbach <joshua.redelbach@t-online.de>
2023-11-30 19:11:05 +0100

    1st Implementation of PID + ReactionwheelControl

---
+++ STM32F4_RODOS_Template/control/PIDController.hpp
@@ -0,0 +1,91 @@
+#ifndef FLOATSAT_CONTROL_PIDCONTROLLER_HPP_
+#define FLOATSAT_CONTROL_PIDCONTROLLER_HPP_
+
+#include "rodos.h"
+#include "matlib.h"
+
+/**
+ * @brief Struct for controller parameters 
+*/
+struct PIDParams {
+    float kp;
+    float ki;
+    float kd;   
+};
+
+
+class PID {
+
+    private:
+
+        /// @brief Controller parameters
+        PIDParams parameters;
+
+        /// @brief Desired value the controller should achieve
+        float setpoint;
+
+        /// @brief Error of last run for determining the derivation of the error
+        float lastError;
+
+        /// @brief Integration of error until last control signal determination
+        float integError;
+
+        /// @brief Timestamp of last control signal determination in nanoseconds            
+        int64_t lastTimestamp;
+
+        /// @brief Limits for control signal
+        float maxLimit;
+        float minLimit;
+
+        /// @brief Indicates if derivation and integration term can be included -> after lastTimestamp is initialized
+        bool flagInitialized = false;
+
+        
+    public:
+
+        /**
+         * @brief Constructor
+        */
+       PID();
+
+
+       /**
+        * @brief Initialize parameters
+        */
+       void init(const PIDParams &params, float maxLimit, float minLimit);
+
+
+        /**
+         * @brief Calculate new control output based on current setpoint and current measurement
+        */
+        float calculate(float measurement, int64_t dt);
+
+
+        /**
+         * @brief Set parameter of controller
+        */
+        void setParams(const PIDParams &params);
+
+
+        /**
+         * @brief Get parameter of controller
+        */
+        PIDParams& getParams();
+
+
+        /**
+         * @brief Set setpoint of controller
+        */
+        void setSetpoint(float setpoint);
+
+
+        /**
+         * @brief Set max. and min. limit of control signal
+        */
+        void setLimits(float maxLimit, float minLimit);
+
+
+};
+
+
+#endif
--- STM32F4_RODOS_Template/control/ReactionwheelControl.cpp
+++ STM32F4_RODOS_Template/control/ReactionwheelControl.cpp
@@ -1,29 +1,47 @@
 #include "ReactionwheelControl.hpp"
+#include "rodos.h"
+
 
 
 ReactionwheelControl::ReactionwheelControl()
 {
-
+    this->controller = PID();
 }
 
-void ReactionwheelControl::setParams(const ReactionwheelControlParams& params)
+
+
+void ReactionwheelControl::init(const PIDParams& params, float maxLimit, float minLimit, float maxVoltage)
 {
-
+    this->controller.init(params, maxLimit, minLimit);
+    this->maxVoltage = maxVoltage;
 }
 
-const ReactionwheelControlParams& ReactionwheelControl::getParams()
+
+
+void ReactionwheelControl::setParams(const PIDParams& params)
 {
-
+    this->controller.setParams(params);
 }
+
+
+
+const PIDParams& ReactionwheelControl::getParams()
+{
+    return this->controller.getParams();
+}
+
 
 void ReactionwheelControl::setDesiredSpeed(float w_set)
 {
-
+    this->controller.setSetpoint(w_set);
 }
 
-float ReactionwheelControl::update(float w_mes)
-{
 
+
+float ReactionwheelControl::update(TimestampedData<float> speed)
+{
+    float controlSignal = this->controller.calculate(speed.data, speed.timestamp);
+    controlSignal = controlSignal / maxVoltage;
 }
 
 ReactionwheelControl reactionwheelControl;
