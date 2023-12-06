ba187ec870b4716b2451f2c8ca547b67a6a85db8
ChSt15 <chrissteffen98@gmail.com>
2023-12-06 10:37:39 +0100

    almost final API update

--- STM32F4_RODOS_Template/hardware/ElectricalMonitoring.cpp
+++ STM32F4_RODOS_Template/hardware/ElectricalMonitoring.cpp
@@ -36,63 +36,105 @@ void ElectricalMonitoring::BeeperThread::run()
 }
 
 
-ElectricalMonitoring::ElectricalMonitoring(RODOS::GPIO_PIN powerOffPin, RODOS::ADC_CHANNEL adcVBATPin, RODOS::ADC_CHANNEL adcShuntPin, RODOS::ADC_CHANNEL adcWheelPin, RODOS::PWM_IDX beeperPin) :
+ElectricalMonitoring::ElectricalMonitoring(RODOS::GPIO_PIN powerOffPin, RODOS::ADC_CHANNEL adcWheelPin, RODOS::PWM_IDX beeperPin, RODOS::I2C_IDX ina3221_i2cBus) :
     beeperThread(beeperPin),
     extPowerPin_(powerOffPin),
-    adcVBATPin_(adcVBATPin),
-    adcShuntPin_(adcShuntPin),
     adcWheelPin_(adcWheelPin),
     beeperIDX_(beeperPin),
-    extPower(powerOffPin),
-    adcVBAT(RODOS::ADC_IDX0),
-    adcShunt(RODOS::ADC_IDX0),
-    adcWheel(RODOS::ADC_IDX0),
-    beeper(beeperPin)
+    extPower_(powerOffPin),
+    adcWheel_(RODOS::ADC_IDX0),
+    beeper_(beeperPin),
+    i2cBus_(ina3221_i2cBus)
 {}
 
 void ElectricalMonitoring::initialize()
 {
-    extPower.init(true, 1, 0);
-    adcVBAT.init(adcVBATPin_);
-    adcShunt.init(adcShuntPin_);
-    adcWheel.init(adcWheelPin_);
+    extPower_.init(true, 1, 0);
+    adcWheel_.init(adcWheelPin_);
 }
 
 void ElectricalMonitoring::update()
 {   
 
-    float lpf_alpha = 0.1f;
+
+
+    switch (state_)
+    {
+    case SystemState_t::INIT :
+        {
+            beeperThread.beepForTime_ns(500*MILLISECONDS);
+        }
+        break;
+
+    case SystemState_t::CONV_INIT :
+        /* code */
+        break;
+
+    case SystemState_t::CONV_WAIT :
+        /* code */
+        break;
+
+    case SystemState_t::POWERDOWN_INIT :
+        /* code */
+        break;
+
+    case SystemState_t::POWERDOWN_WAIT :
+        /* code */
+        break;
+
+    case SystemState_t::POWERDOWN_COMPLETE :
+        /* code */
+        break;
     
-    if (firstRun) {
-        firstRun = false;
-
-        beeperThread.beepForTime_ns(500*MILLISECONDS);
-
-        lpf_alpha = 1.0f; // no LPF on first run
-
+    default:
+        state_ = SystemState_t::INIT;
+        break;
     }
-
-    // Read ADCs
-    //float adcVBAT_raw = adcVBAT.read();
-    //float adcShunt_raw = adcShunt.read();
-    
 
 }	
 
+void ElectricalMonitoring::readValues(bool setValue) {
+
+}
+
 float ElectricalMonitoring::getBatteryVoltage() {
-    return 0;
+    return voltageBattery_;
 }
 
 float ElectricalMonitoring::getAuxCurrent() {
-    return 0;
+    return currentAux_;
 }
 
 float ElectricalMonitoring::getReactionWheelCurrent() {
-    return 0;
+    return currentReactionWheel_;
 }
 
-void ElectricalMonitoring::powerOff() {
-    
+float ElectricalMonitoring::get5VBusVoltage() {
+    return voltage5VBus_;
+}
+
+float ElectricalMonitoring::getRPICurrent() {
+    return currentRPI_;
+}
+
+float ElectricalMonitoring::getStepperCurrent() {
+    return currentStepper_;
+}
+
+void ElectricalMonitoring::powerDown() {
+    //return 0;
+}
+
+void ElectricalMonitoring::openExtSwitch() {
+    //return 0;
+}
+
+bool ElectricalMonitoring::powerGood() {
+    return powerGood_;
+}
+
+bool ElectricalMonitoring::rpiRunning() {
+    return rpiRunning_:
 }
 
 
