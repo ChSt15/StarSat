73280fcd9576fcbc8331dda89f4dc79a404affd6
DerDax7 <maxi.roth@me.com>
2023-12-12 17:14:35 +0100

    fixes

--- STM32F4_RODOS_Template/Threads/Control_Thread.cpp
+++ STM32F4_RODOS_Template/Threads/Control_Thread.cpp
@@ -46,7 +46,6 @@ void ControlThread::run()
 	TimestampedData<Attitude_Data> AttitudeDataReceiver;
 	TimestampedData<float> EncoderDataReceiver;
 
-	// Helper var
 	float desiredVelocity;
 	float desiredSpeed;
 	float desiredVoltage;
