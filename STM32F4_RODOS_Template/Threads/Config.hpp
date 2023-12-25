7329248c24a2bbb8891e5669294e207dce865949
DerDax7 <maxi.roth@me.com>
2023-12-25 01:00:45 +0100

    made config vars inline

--- STM32F4_RODOS_Template/Threads/Config.hpp
+++ STM32F4_RODOS_Template/Threads/Config.hpp
@@ -9,63 +9,63 @@ namespace config
 
     /* ----------------------------------------- Threads -----------------------------------------  */
     // Control
-    static const bool control_thread_enable = true;
-    static const int  control_thread_period = 100;     // [ms]
+    inline const bool control_thread_enable = true;
+    inline const int  control_thread_period = 100;     // [ms]
 
     // Sensor
-    static const bool sensor_thread_enable = true;
-    static const int  sensor_thread_period = 50;       // [ms]
+    inline const bool sensor_thread_enable = true;
+    inline const int  sensor_thread_period = 50;       // [ms]
 
     // Coms
-    static const bool com_thread_enable = true;
-    static const int  com_thread_period = 200;         // [ms]
+    inline const bool com_thread_enable = true;
+    inline const int  com_thread_period = 200;         // [ms]
 
     // Debug
-    static const bool debug_thread_enable = false;
-    static const int  debug_thread_period = 500;       // [ms]
+    inline const bool debug_thread_enable = true;
+    inline const int  debug_thread_period = 500;       // [ms]
 
     /* ---------------------------------------- Hardware ---------------------------------------  */
     // IMU
-    static const IMUCalib gyroCalib{
+    inline const IMUCalib gyroCalib{
         Vector3D_F(0.011274, 0.018649, -0.021776),                      // Offset [rad/s]
         Matrix3D_F(YPR_F(0, 0, 0)) };                                   // Yaw, Pitch, Roll [rad]
-    static const IMUCalib accelCalib{
+    inline const IMUCalib accelCalib{
         Vector3D_F(-0.025037, -0.0051067, -0.0089848),                  // Offset [g]
         Matrix3D_F(YPR_F(0, 0, 0)) };                                   // Yaw, Pitch, Roll [rad]
-    static const IMUCalib magCalib{
+    inline const IMUCalib magCalib{
         Vector3D_F(-0.025, 0.2625, -0.0495),                            // Offset [gauss]
         Matrix3D_F(YPR_F(0, 0, 0)) };                                   // Yaw, Pitch, Roll [rad]
 
     // HBridge
-    static const int pwmFrequency = 2000;       // [Hz]
-    static const int pwmIncrements = 500;
+    inline const int pwmFrequency = 2000;       // [Hz]
+    inline const int pwmIncrements = 500;
 
     /* ---------------------------------------- Control ----------------------------------------  */
     // Speed Controller
-    static const float limitSpeedController = 1.f;                    //
-    static const PIDParams paramsSpeedControl{ 1.0f, 1.0f, 1.0f };      // P, I, D
+    inline const float limitSpeedController = 1.f;                      //
+    inline const PIDParams paramsSpeedControl{ 1.0f, 1.0f, 1.0f };      // P, I, D
     // Position Controller
-    static const float limitPosController = 1.f;                        //
-    static const PIDParams paramsPosController{ 1.0f, 1.0f, 1.0f };     // P, I, D
+    inline const float limitPosController = 1.f;                        //
+    inline const PIDParams paramsPosController{ 1.0f, 1.0f, 1.0f };     // P, I, D
     // Velocity Controller  
-    static const float limitVelController = 1.f;                        // 
-    static const PIDParams paramsVelController{ 1.0f, 1.0f, 1.0f };     // P, I, D
+    inline const float limitVelController = 1.f;                        // 
+    inline const PIDParams paramsVelController{ 1.0f, 1.0f, 1.0f };     // P, I, D
 
     // Arm Controller
-    static const int max_vel = 50;			// [step/s]
-    static const int min_vel = 1;			// [step/s]
-    static const int max_accel = 5;		    // [step/s^2]
+    inline const int max_vel = 50;			// [step/s]
+    inline const int min_vel = 1;			// [step/s]
+    inline const int max_accel = 5;		    // [step/s^2]
 
     // IMU Calibration
-    static int gyro_maxsamples = 200;
-    static int accel_maxsamples = 200;
-    static int mag_maxsamples = 500;
+    inline const int gyro_maxsamples = 200;
+    inline const int accel_maxsamples = 200;
+    inline const int mag_maxsamples = 500;
 
     // QEKF
-    static const Vector3D_F sigma_gyro = Vector3D_F(0.0027728, 0.0023483, 0.0018954);      // [rad/s]
-    static const Vector3D_F sigma_accel = Vector3D_F(0.0014530, 0.0017393, 0.0044343);     // [g]
-    static const float sigma_yaw = 0.018469;                                               // [rad]
-    static const float sigma_gyro_drift = 1.0f * pow(10, -6);                              // [rad/s^2]
+    inline const Vector3D_F sigma_gyro = Vector3D_F(0.0027728, 0.0023483, 0.0018954);      // [rad/s]
+    inline const Vector3D_F sigma_accel = Vector3D_F(0.0014530, 0.0017393, 0.0044343);     // [g]
+    inline const float sigma_yaw = 0.018469;                                               // [rad]
+    inline const float sigma_gyro_drift = 1.0f * pow(10, -6);                              // [rad/s^2]
 
     /* ------------------------------------------ Com -----------------------------------------  */
 
