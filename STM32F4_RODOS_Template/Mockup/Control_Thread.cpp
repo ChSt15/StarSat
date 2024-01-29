ead9e3138e4d803eba301e5b7c32cb2e08442952
Joshua Redelbach <joshua.redelbach@t-online.de>
2024-01-29 17:15:33 +0100

    mockuo fix

--- STM32F4_RODOS_Template/Mockup/Control_Thread.cpp
+++ STM32F4_RODOS_Template/Mockup/Control_Thread.cpp
@@ -22,13 +22,25 @@ void ControlThread::init()
 
 void ControlThread::run()
 {
-	float desiredVoltage;
+	float desiredVoltage_percent;
 
 	while(true)
 	{
-		desiredVoltage = adc.getVoltage();
-		// hbridge.setVoltage(desiredVoltage);
-		PRINTF("Desired Voltage:    %f\n", desiredVoltage);
+		desiredVoltage_percent = adc.getVoltage() / 3000.f;
+
+		if (desiredVoltage_percent > 0.95) desiredVoltage_percent = 1.f;
+		else if (desiredVoltage_percent > 0.75) desiredVoltage_percent = 0.8f;
+		else if (desiredVoltage_percent > 0.55) desiredVoltage_percent = 0.6f;
+		else if (desiredVoltage_percent > 0.35) desiredVoltage_percent = 0.4f;
+		else if (desiredVoltage_percent > 0.15) desiredVoltage_percent = 0.2f;
+		else desiredVoltage_percent = 0.f;
+
+		desiredVoltage_percent *= 0.2;
+
+
+
+		hbridge.setVoltage(desiredVoltage_percent);
+		//PRINTF("Desired Voltage:    %f\n", desiredVoltage_percent);
 
 		suspendCallerUntil(NOW() + period * MILLISECONDS);
 	}
