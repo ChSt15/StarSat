7f2d3f0d60429a9f07630f1a6feab576f2c15667
ChSt15 <chrissteffen98@gmail.com>
2024-02-12 09:26:29 +0100

    Change posControl to use Quat or deriv param

--- STM32F4_RODOS_Template/OuterLoop/AngularPositionControl.cpp
+++ STM32F4_RODOS_Template/OuterLoop/AngularPositionControl.cpp
@@ -25,7 +25,21 @@ void AngularPositionControl::setSetpoint(float angle_set)
 
 float AngularPositionControl::update(TimestampedData<Attitude_Data> attitude_measured)
 {
-    float measured_pos = attitude_measured.data.attitude.toYPR().yaw;
+
+    auto xAxis = Vector3D(1, 0, 0);
+    auto zAxis = Vector3D(0, 0, 1);
+    auto angle = PID::getSetpoint();
+    auto setpointQ = Quaternion(AngleAxis(angle, zAxis));
+
+    auto errorQ = attitude_measured.data.attitude*(setpointQ.conjugate());
+
+    auto errorYaw = errorQ.toYPR().yaw;
+
+    PID::setSetpoint(0);
+    return PID::calculate(errorYaw, attitude_measured.timestamp);
+
+    
+    /*float measured_pos = attitude_measured.data.attitude.toYPR().yaw;
 
     // make sure the angle "warp around" by limiting set angle to measured angle +/- pi;
     float set_pos = PID::getSetpoint();
@@ -33,7 +47,7 @@ float AngularPositionControl::update(TimestampedData<Attitude_Data> attitude_mea
     while (set_pos < measured_pos - M_PI) set_pos += 2 * M_PI;
     PID::setSetpoint(set_pos);
 
-    return PID::calculate(measured_pos, attitude_measured.timestamp);
+    return PID::calculate(measured_pos, attitude_measured.data.angularVelocity.z, attitude_measured.timestamp);*/
 }
 
 
